1d0755932f9d39315ac1587fca8c37358dd0b5b6
Eivind <eivind.m.bakke@gmail.com>
2016-01-29 16:15:12 -0600

    added favicon

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
-    secret: 'PlaneChatSuperSecret',
+    secret: 'NudlSecret',
     resave: false,
     saveUninitialized: false//,
     // store: new MongoStore({
