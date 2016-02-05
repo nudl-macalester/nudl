4f049a0f6cc9b24d5914bfd45c3c41e1c27f62af
Devin Bjelland <dbjellan@macalester.edu>
2016-02-05 10:52:13 -0600

    Changed db name back

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-mongoose.connect('mongodb://localhost:27017/nudl', function(err, db) {
+mongoose.connect('mongodb://localhost:27017/NUDL', function(err, db) {
 	if (err) console.log("Failed to connect to mongodb: " + err)
 	else {
 		console.log("Connected to db")
