0795425b4c386869924d2e9be75ce4e1476888e5
adangel29 <adangel@macalester.edu>
2016-02-23 16:52:40 -0600

    added emailtemplates file

---
+++ emailTemplates.js
@@ -0,0 +1,8 @@
+
+module.exports.getReminderTemplate = function(user, mealshare) {
+	var reminderBody = "Hi there" +
+		user.name +
+		"<br>" +
+		"remember your mealshare:";
+	return reminderBody;
+}
